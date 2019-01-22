07d71a19ef456d9aefdba4adcc6781ae4e32b490
Tymec <tymek1rt@hotmail.com>
2019-01-22 09:58:55 +0100

    Update List.py

--- modules/List.py
+++ modules/List.py
@@ -7,8 +7,9 @@ from .Helper import MediaType
 
 
 class List:
-    def __init__(self, website):
+    def __init__(self, website, output_format='json'):
         self.website = website
+        self.output_format = output_format
 
 
 class MovieList(List):
@@ -71,3 +72,48 @@ class ComicList(List):
 
         response = requests.get(f"{ self.website.api_url }/search/", params=variables, headers=headers).json()
         return response
+        
+
+class GameList(List):
+    def __init__(self, api_key=None):
+        _website = Source("IGDB", MediaType.GAME, "https://igdb.com", "https://api-v3.igdb.com", api_key)
+        super().__init__(_website)
+
+    def getUserList(self, user_name):
+        return { 'return': 'Not yet implemented' }
+
+    def getEntry(self, entry_id):
+        return { 'return': 'Not yet implemented' }
+
+    def searchEntry(self, search_input, page_number, parameters):
+        return { 'return': 'Not yet implemented' }
+
+
+class BookList(List):
+    def __init__(self, api_key=None):
+        _website = Source("Goodreads", MediaType.BOOK, "https://www.goodreads.com", "https://www.goodreads.com", api_key)
+        super().__init__(_website, 'xml')
+
+    def getUserList(self, user_name):
+        return { 'return': 'Not yet implemented' }
+
+    def getEntry(self, entry_id):
+        return { 'return': 'Not yet implemented' }
+
+    def searchEntry(self, search_input, page_number, parameters):
+        return { 'return': 'Not yet implemented' }
+        
+
+class MusicList(List):
+    def __init__(self, api_key=None):
+        _website = Source("Spotify", MediaType.MUSIC, "https://spotify.com", "https://api.spotify.com", api_key)
+        super().__init__(_website)
+
+    def getUserList(self, user_name):
+        return { 'return': 'Not yet implemented' }
+
+    def getEntry(self, entry_id):
+        return { 'return': 'Not yet implemented' }
+
+    def searchEntry(self, search_input, page_number, parameters):
+        return { 'return': 'Not yet implemented' }
